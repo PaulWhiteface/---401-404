9212da0e34028de8344d4a06fc24736ee4a7aa15
Paul Belikov <paul.belikov@mail.ru>
2024-06-07 17:41:20 +0300

    all pages ready - not pixelperfect

--- errorpages/src/pages/NotFound.vue
+++ errorpages/src/pages/NotFound.vue
@@ -15,15 +15,110 @@
 export default {
   name: 'NotFound',
   props: {
-    
+
   }
 }
 </script>
 
 <style>
+div,
+h1,
+h2,
+button {
+  outline: 1px solid red;
+}
 
-div, h1, h2, button {
-    outline: 1px solid red;
+  .not-found {
+    background-color: #F2F1EC;
+    background-image: url('../assets/img/NotFoundBackgroundLarge.svg');
+    background-repeat: no-repeat;
+    background-position: top right;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: #414143;
+    padding: 0 60px;
+  }
+
+  .not-found__title {
+    margin-top: 150px;
+    margin-bottom: 50px;
+    font-family: Unbounded;
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 57.6px;
+    text-align: center;
+  }
+
+  .not-found__picture {
+    max-width: 1290px;
+    max-height: 520px;
+  }
+
+  .not-found__image {
+    max-width: 100%;
+    max-height: 100%;
+  }
+
+  .not-found__button {
+    margin-top: 50px;
+    max-width: 327px;
+    max-height: 73px;
+    font-family: Nunito Sans;
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 32.74px;
+    padding: 20px 32px;
+    white-space: nowrap;
+    background-color: #F2F1EC;
+    border: 2px solid #C9FF22;
+    border-radius: 50px;
+    cursor: pointer;
+    color: #414143;
+  }
+
+  @media screen and (max-width: 1100px) {
+    .not-found__title {
+      font-size: 26px;
+      line-height: 31.2px;
+    }
+
+    .not-found__button {
+      padding: 14px 20px;
+      font-size: 20px;
+      line-height: 27.28px;
+      max-width: 259px;
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    .not-found {
+      background-image: url('../assets/img/NotFoundBackgroundMedium.svg')
+    }
+
+  }
+
+  @media screen and (max-width: 500px) {
+    .not-found {
+      padding: 0 20px;
+    }
+
+    .not-found__button {
+      max-width: 320px;
+      padding: 20px 32px;
+    }
+  }
+
+  @media screen and (max-width: 360px) {
+    .not-found {
+      background-image: url('../assets/img/NotFoundBackgroundSmall.svg');
+    }
+
+    .not-found__title {
+      max-width: 240px;
+    }
+
   }
 
 </style>
